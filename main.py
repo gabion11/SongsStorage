47b80cb17b65973cfbb22d4e43a8eeae5fa20a2c
Gitea <tcaciu.ionut@gmail.com>
2020-12-10 22:29:49 +0200

    10-12-2020
    removed some unnecessary files

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     meta = MetadataController()
     # meta.delete_metadata(1)
     # meta.insert_metadata("Storage", "FS", "Summner", date(1970, 10, 8), "#hello")
-    songs = meta.update_file_name(2, {"FileName": "storage", "Artist": "Vl"})
+    songs = meta.update_metadata(2, {"FileName": "storage", "Artist": "Vl"})
     meta.print_metadata(songs)
     songs = meta.get_metadata()
     for song in songs:
